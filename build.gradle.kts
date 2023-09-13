7fc54277f8e32d6efb36938d1e88a0b791dab104
Aria <25481147+AriaAdibi@users.noreply.github.com>
2023-09-13 10:39:22 -0400

    Spring Security Init

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.1.3"
+	id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "com.testingground"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    val jJWTVersin = "0.11.5"
+	implementation("io.jsonwebtoken:jjwt-api:$jJWTVersin")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:$jJWTVersin")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jJWTVersin")
+
+	implementation("javax.xml.bind:jaxb-api:2.3.1")
+
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	annotationProcessor("org.projectlombok:lombok")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
