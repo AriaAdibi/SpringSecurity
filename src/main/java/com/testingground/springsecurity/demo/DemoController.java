54704bba29a72021c14a19d4a7ddb4f0de8c1cbf
Aria <25481147+AriaAdibi@users.noreply.github.com>
2023-09-14 10:49:02 -0400

    + Demo API

---
+++ src/main/java/com/testingground/springsecurity/demo/DemoController.java
@@ -0,0 +1,20 @@
+package com.testingground.springsecurity.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("api/v1/demo")
+public class DemoController {
+
+  @GetMapping("welcome-user")
+  public ResponseEntity<String> welcomeUser() {
+    return ResponseEntity.ok("Welcome User!");
+  }
+
+  @GetMapping("welcome-admin")
+  public ResponseEntity<String> welcomeAdmin() {
+    return ResponseEntity.ok("Welcome Admin!");
+  }
+
+}
